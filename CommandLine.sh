c94341e6414df6cc0344958838f12190440e474e
cem-sirin <sirincem1@gmail.com>
2022-12-02 12:40:53 +0100

    Run the entire file
    so that it looks nice :100:

--- CommandLine.sh
+++ CommandLine.sh
@@ -1,13 +1,12 @@
 echo '1-The location that has the maximum number of purchases been made:'
 
-awk -F, '{print($5)}' bank_transactions.csv | sort | uniq -c | sort -nr | head -1
+awk -F, '{print($5)}' data/bank_transactions.csv | sort | uniq -c | sort -nr | head -1
 
 echo '2-The most spending purchases are: '
 
-awk -F, '{$4+=$9}END{ if(list["F"] > list["M"]){ print("F");} else{ print("M");}}' bank_transactions.csv
+awk -F, '{$4+=$9}END{ if(list["F"] > list["M"]){ print("F");} else{ print("M");}}' data/bank_transactions.csv
 
 echo '3-The customer with the highest average transaction amount: '
 
-awk -F, '{c[$2]++;list[$2]+=$9}END{for (i in list) print(list[i]/c[i],i)}' bank_transactions.csv | sort -nr | head -1
-
+awk -F, '{c[$2]++;list[$2]+=$9}END{for (i in list) print(list[i]/c[i],i)}' data/bank_transactions.csv | sort -nr | head -1
 
