6db155343641c7f61f154c99c73a6a8d6cdf4917
suka94 <52387382+suka94@users.noreply.github.com>
2022-12-07 13:47:15 +0100

    Update CommandLine.sh

--- CommandLine.sh
+++ CommandLine.sh
@@ -6,6 +6,7 @@ echo '2-The most spending purchases are: '
 
 awk -F, '{$4+=$9}END{ if(list["F"] > list["M"]){ print("F");} else{ print("M");}}' data/bank_transactions.csv
 
-awk -F, 'BEGIN{FS=","} {if(NR>1){arr[$2]= arr[$2]+$9; count[$2]++}} END{for(c in arr){arr[c]= arr[c]/count[c]} asort(arr, sortedarr);for(l in arr){if(arr[l]==sortedarr[length(sortedarr)])print "customer with the highest average",l, arr[l]}}' bank_transactions.csv
+echo '3-The customer with the highest average transaction amount: '
 
+awk -F, 'BEGIN{FS=","} {if(NR>1){arr[$2]= arr[$2]+$9; count[$2]++}} END{for(c in arr){arr[c]= arr[c]/count[c]} asort(arr, sortedarr);for(l in arr){if(arr[l]==sortedarr[length(sortedarr)])print arr[l], l}}' bank_transactions.csv
 
